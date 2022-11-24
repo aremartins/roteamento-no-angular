90e8c3ced447e9c72c748399254e58fd5a287ca5
aremartins <arealbmartinsan@gmail.com>
2022-11-24 09:21:11 -0300

    feat: core service

---
+++ src/app/core/services/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  user: User = new User()
+
+  showMenuEmitter = new EventEmitter<boolean>()
+
+  userAuth: boolean = false
+
+  constructor(private router: Router) { }
+
+
+  login(user: User): void{
+    if(user.usuario === 'admin'.toLocaleLowerCase().trim() && Number(user.senha) === 123456){
+      this.user = user
+      this.userAuth = true
+      this.showMenuEmitter.emit(true)
+      this.router.navigate(['/'])
+    } else {
+      this.userAuth = false
+      this.showMenuEmitter.emit(false)
+      this.router.navigate(['/login'])
+    }
+  }
+}
